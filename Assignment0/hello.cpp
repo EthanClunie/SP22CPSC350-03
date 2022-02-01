a7a88f13e15e4f7889b302e2859032c2654da13a
Gamma217 <eclunie@chapman.edu>
2022-02-01 11:21:10 -0800

    Main executable of Assignment 0

---
+++ Assignment0/hello.cpp
@@ -0,0 +1,10 @@
+/// @file hello.cpp
+/// @brief Outputs Hello World!
+/// @author Ethan Clunie
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
